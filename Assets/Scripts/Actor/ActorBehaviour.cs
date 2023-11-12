4b1ac05ba3fb7f711c7960e2cb879db93414028d
Celezt <59172226+Celezt@users.noreply.github.com>
2023-11-12 22:22:14 +0100

    Add actor rotation

--- Assets/Scripts/Actor/ActorBehaviour.cs
+++ Assets/Scripts/Actor/ActorBehaviour.cs
@@ -11,6 +11,11 @@ public class ActorBehaviour : MonoBehaviour
     /// Direction in world space.
     /// </summary>
     public Vector3 Direction => _direction;
+    /// <summary>
+    /// Direction the actor should looking towards. Is never zero.
+    /// </summary>
+    public Vector3 LookDirection => _lookDirection;
+    public bool IsMoving => _isMoving;
     public float MoveForce
     {
         get => _moveForce;
@@ -27,18 +32,24 @@ public class ActorBehaviour : MonoBehaviour
     [SerializeField]
     private TriggerHandler _trigger;
     [SerializeField]
+    private float _rotationSpeed = 8f;
+    [SerializeField]
     private float _moveForce = 40f;
     [SerializeField]
     private float _dragCoefficientHorizontal = 4f;
     [SerializeField]
     private AnimationCurve _initialForceCurve = AnimationCurve.EaseInOut(0, 20, 1, 0);
+    [SerializeField, Range(0, 360)]
+    private float _angleDifferenceForInitial = 80;
     [SerializeField]
     private AnimationCurve _stopForceCurve = AnimationCurve.EaseInOut(0, 0, 1, 20);
     [SerializeField]
     private Coordinates _coordinate = Coordinates.World;
 
     private CancellationTokenSource _cancellationTokenSource;
+    private UniTask.Awaiter _initialForceAwaiter;
     private Vector3 _direction;
+    private Vector3 _lookDirection;
     private bool _isMoving;
 
     public enum Coordinates
@@ -67,33 +78,38 @@ public class ActorBehaviour : MonoBehaviour
                 break;
         }
 
-        if (_direction == Vector3.zero)
-        {
-            _isMoving = false;
-        }
-        else
-        {
-            if (!_isMoving)
-            {
-                CTSUtility.Reset(ref _cancellationTokenSource);
-                InitialForceAsync(_cancellationTokenSource.Token).Forget();
-            }
+        _isMoving = _direction != Vector3.zero;
 
-            _isMoving = true;
+        if (_isMoving)  // Only update when a direction exist.
+            _lookDirection = _direction;
+
+        float angle = Vector3.Angle(_rigidbody.transform.forward, _direction);
+        if (angle > _angleDifferenceForInitial && _initialForceAwaiter.IsCompleted)
+        {
+            CTSUtility.Reset(ref _cancellationTokenSource);
+            _initialForceAwaiter = InitialForceAsync(_cancellationTokenSource.Token).GetAwaiter();
         }
+    }
+
+    private void Start()
+    {
+        _lookDirection = transform.forward;
     }
 
     private void FixedUpdate()
     {
+        float deltaTime = Time.deltaTime;
+
         if (!_trigger?.IsTriggered ?? false)  // Don't move if it actor is not on the ground.
-        {
             _direction = Vector3.zero;
-            return;
-        }
 
         Vector3 dragForce = _direction != Vector3.zero ? 
             -_dragCoefficientHorizontal * _rigidbody.velocity.x_z() : Vector3.zero;
         Vector3 totalMoveForce = (_direction * _moveForce) + dragForce;
+
+        Quaternion lookRotation = Quaternion.LookRotation(_lookDirection, Vector3.up);
+        Quaternion rotation = Quaternion.Slerp(_rigidbody.rotation, lookRotation, deltaTime * _rotationSpeed);
+        _rigidbody.MoveRotation(rotation);
 
         _rigidbody?.AddForce(totalMoveForce);
     }
@@ -103,13 +119,14 @@ public class ActorBehaviour : MonoBehaviour
         CTSUtility.Clear(ref _cancellationTokenSource);
     }
 
-    private async UniTaskVoid InitialForceAsync(CancellationToken cancellationToken)
+    private async UniTask InitialForceAsync(CancellationToken cancellationToken)
     {
         if (_initialForceCurve.length == 0) // There is no keys.
             return;
 
         float maxDuration = _initialForceCurve[_initialForceCurve.length - 1].time;
         float startTime = Time.time;
+        Vector3 startDirection = _direction;
 
         while (!cancellationToken.IsCancellationRequested)
         {
@@ -119,8 +136,11 @@ public class ActorBehaviour : MonoBehaviour
             if (duration >= maxDuration)
                 break;
 
+            if (_direction == Vector3.zero)
+                startDirection = Vector3.zero;
+
             float force = _initialForceCurve.Evaluate(duration);
-            _rigidbody.AddForce(_direction * force);
+            _rigidbody.AddForce(startDirection * force);
 
             await UniTask.WaitForFixedUpdate(cancellationToken);
         }
