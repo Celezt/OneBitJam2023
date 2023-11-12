58adb44d93cb52ce02f0ca01a0e6d7b3a6f00b54
Celezt <59172226+Celezt@users.noreply.github.com>
2023-11-12 20:37:46 +0100

    Add button to enable and disable debug

--- Assets/Scripts/UI/UIDebug.cs
+++ Assets/Scripts/UI/UIDebug.cs
@@ -9,6 +9,10 @@ using UnityEngine.InputSystem;
 public class UIDebug : MonoBehaviour
 {
     [SerializeField]
+    private GameObject _container;
+    [SerializeField]
+    private bool _isActiveByDefault = false;
+    [SerializeField]
     private TextMeshProUGUI _velocityText;
 
     private PlayerInput _playerInput;
@@ -16,13 +20,36 @@ public class UIDebug : MonoBehaviour
 
     private void Start()
     {
+        _container?.SetActive(_isActiveByDefault);
+
         _playerInput = PlayerInput.GetPlayerByIndex(0);
         _rigidbody = _playerInput.GetComponentInParent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        InputSystem.actions["Debug"].started += OnDebug;
+    }
+
+    private void OnDisable()
+    {
+        InputSystem.actions["Debug"].started -= OnDebug;
     }
 
     private void FixedUpdate()
     {
         if (_velocityText != null && _rigidbody != null)
             _velocityText.text = _rigidbody.velocity.magnitude.ToString("0.00") + " m/s";
+    }
+
+    private void OnDebug(InputAction.CallbackContext context)
+    {
+        if (_container == null)
+            return;
+
+        if (_container.activeSelf)
+            _container.SetActive(false);
+        else
+            _container.SetActive(true);
     }
 }
