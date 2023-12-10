2447fb44f0448e4318403f3111cf01005fe0e5e8
Celezt <59172226+Celezt@users.noreply.github.com>
2023-12-10 03:50:04 +0100

    Fix blending issues between animations

--- Assets/Scripts/Utilities/Processors/ClampProcessorVector.cs
+++ Assets/Scripts/Utilities/Processors/ClampProcessorVector.cs
@@ -3,13 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClampProcessorVector : IProcessor<Vector2>
+public class NormalizeProcessorVector : IProcessor<Vector2>
 {
-    [SerializeField]
-    private float _minValue;
-    [SerializeField] 
-    private float _maxValue;
-
     public Vector2 Process(Vector2 value)
-        => new Vector2(Mathf.Clamp(value.x, _minValue, _maxValue), Mathf.Clamp(value.y, _minValue, _maxValue));
+        => value.normalized;
 }
