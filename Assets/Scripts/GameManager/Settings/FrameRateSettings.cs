fcdf4ddd3abf8437129e728ac1a0995389d6582c
Celezt <59172226+Celezt@users.noreply.github.com>
2023-12-29 19:54:42 +0100

    Added virtual follow camera prefab

--- Assets/Scripts/GameManager/Settings/FrameRateSettings.cs
+++ Assets/Scripts/GameManager/Settings/FrameRateSettings.cs
@@ -11,10 +11,13 @@ public class FrameRateSettings : ISettings
     private bool _fixedFrameRate = false;
     [SerializeField, EnableIf(nameof(_fixedFrameRate)), Indent, OnValueChanged(nameof(OnSetFrameRate))]
     private int _frameRate = 60;
+    [SerializeField, OnValueChanged(nameof(OnSetVSync)), LabelText("V-Sync")]
+    private bool _vSync = false;
     
     public void GameStart(IEnumerable<ISettings> settings)
     {
         OnSetFrameRate();
+        OnSetVSync();
     }
 
     public void GameExit(IEnumerable<ISettings> settings)
@@ -28,5 +31,16 @@ public class FrameRateSettings : ISettings
             Application.targetFrameRate = _frameRate;
         else
             Application.targetFrameRate = -1;
+    }
+
+    private void OnSetVSync()
+    {
+        if (_vSync)
+        {
+            float vSyncFactor = (float)Screen.currentResolution.refreshRateRatio.value / 60.0f;
+            QualitySettings.vSyncCount = Mathf.Clamp(Mathf.RoundToInt(vSyncFactor), 1, 4);
+        }
+        else
+            QualitySettings.vSyncCount = 0;
     }
 }
